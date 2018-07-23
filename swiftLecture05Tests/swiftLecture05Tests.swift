b1fe336721d4b3328c6cf086a1b3de3a15e9df5b
zuobangning <zuobangning@163.com>
2018-07-23 16:31:00 +0800

    111

---
+++ swiftLecture05Tests/swiftLecture05Tests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftLecture05Tests.swift
+//  swiftLecture05Tests
+//
+//  Created by stu1 on 2018/7/23.
+//  Copyright © 2018年 tripim. All rights reserved.
+//
+
+import XCTest
+@testable import swiftLecture05
+
+class swiftLecture05Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
